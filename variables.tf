7673b5c8b9afebb4ff7973c2d4d2785a6b5c1bfc
dhivagar_s <78633631+dhivagar98@users.noreply.github.com>
2023-05-17 14:50:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 variable "gcp_project_id" {
   type = string
   description = "The id of th eproject in which the resource belongs"
+  default = "<your project id>"
 }
+variable "gcp_region" {
+  type = string 
+  default = "us-central"
+}
+
