0761c047d480166cb031d4ec2db08b1c2144b055
dhivagar_s <78633631+dhivagar98@users.noreply.github.com>
2023-05-17 14:34:58 +0530

    Update sample.tf

--- sample.tf
+++ sample.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  credentials = file ("path to SA key")
+  credentials = file ("<path to SA key file>")
   project = "<project id>"
-  region = "<select_region>"
+  region = "us-central1"
 }
 
 # Create GCP storage bucket
@@ -52,6 +52,8 @@ resource "google_compute_router" "my_router" {
     content {
       name = interface.key
       ip_range = cidrsubnet(google_compute_network.my_vpc.ip_cidr_range,8,count.index)
+      linked_vpn_tunnel = null
+      linked_interconnect = null
       linked_vpc = google_compute_network.my_vpc.self_link
     }
   }
@@ -66,4 +68,55 @@ resource "google_compute_subnetwork" "my_subnet" {
 }
 
 # create a security group for each subnet
+resource "google_compute_firewall" "my_security_group" {
+  count = 3
+  name = "dhiva_security_group-${count.index + 1}"
+  network = google_compute_network.my_vpc.self_link
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
 
+  # apply security group to the subnet
+  target_tags = [google_compute_network.my_subnet[count.index].name]
+  
+}
+
+# create a web server instance
+resource "google_compute_instance" "web_server" {
+  count = 2
+  name = "web-server-${count.index + 1}"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-a"
+  boot_disk {
+    intialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.my_vpc.self_link
+    access_config {
+      #allocate public ip address for each web server instance 
+    }
+  }
+}
+
+# create database server instance
+resource "google_compute_instance" "db_server" {
+  name = "db-server"
+  machine_type = "n1-standard-2"
+  zone = "us-central1-a"
+   boot_disk {
+    intialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.my_vpc.self_link
+    access_config {
+      #allocate public ip address for each web server instance 
+    }
+  }
+}
+      
